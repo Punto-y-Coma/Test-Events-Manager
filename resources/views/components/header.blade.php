7d085db7786b64215366d8c20f274c7acca7d496
elegarmi <elegarmi@gmail.com>
2022-01-27 14:21:20 +0100

    Header changed to component

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,17 @@
+<a href="{{ url('/home') }}" class="brand">
+    <h1>Factoría Masterclasses</h1>
+</a>
+
+@if (Route::has('login'))
+    <div class="hidden sm:block navigation">
+        @auth
+            <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+        @else
+            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Inicia sesión</a>
+
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Regístrate</a>
+            @endif
+        @endauth
+    </div>
+@endif
