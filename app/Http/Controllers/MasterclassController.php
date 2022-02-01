816283460fc46d3b7111289158fd9f88b0a0e7d4
EmmaFC <emmafecor@hotmail.com>
2022-02-01 12:42:00 +0100

    controller adding variable

--- app/Http/Controllers/MasterclassController.php
+++ app/Http/Controllers/MasterclassController.php
@@ -7,6 +7,8 @@ use App\Models\Masterclass;
 class MasterclassController extends Controller
 {
     public function index(){
-        return view('welcome');
+        $datos_finalizadas ["masterclasses_out_date"] = Masterclass::where('date', '<', date("Y-m-d"))->paginate(3);
+        return view ('welcome', $datos, $datos_finalizadas);
+       
     }
 }
