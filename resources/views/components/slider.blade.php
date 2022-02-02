3981925600944e71743f17a96fe0ef45256a4494
franjavi01 <fjsg0001@gmail.com>
2022-02-02 13:00:33 +0100

    add slider

---
+++ resources/views/components/slider.blade.php
@@ -0,0 +1,44 @@
+<div>
+  <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active" data-bs-interval="10000">
+        <img src="https://lh4.googleusercontent.com/1uvTzxlJ-rgecLm5vw_vff_-cn0gBzhPfnvYyRBHJNdlfGg7ucvy4VC198o=w2400" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+          <button type="button" class="btn btn-dark">Inscríbete</button>
+        </div>
+      </div>
+      <div class="carousel-item" data-bs-interval="2000">
+        <img src="https://lh5.googleusercontent.com/2VvGz1WnVaoxUOTTwWm_T1dQ9G3dOQ8uGoBwLOfQdXwHqFBFgxOA2Z5dlUU=w2400" class="d-block w-100" alt="..." >
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Second slide label</h5>
+          <p>Some representative placeholder content for the second slide.</p>
+          <button type="button" class="btn btn-dark">Inscríbete</button>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="https://lh5.googleusercontent.com/kpz34_A-tN9MyA12hBsxQi2-JacWAp8RYprezWrzH38yN6lz8Jyrpr63ztM=w2400" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Third slide label</h5>
+          <p>Some representative placeholder content for the third slide.</p>
+          <button type="button" class="btn btn-dark">Inscríbete</button>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</div>
+    
