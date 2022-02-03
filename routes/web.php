18285a9f7758d3aaadebbca1b025a81090a0c3c3
elegarmi <elegarmi@gmail.com>
2022-02-03 11:40:11 +0100

    Working header dropdown

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MasterclassController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -17,16 +16,11 @@ use App\Http\Controllers\MasterclassController;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Auth::routes();
 
-Route::get('/', [MasterclassController::class, 'index'])->name('welcome');
 Route::get('/home', [HomeController::class, 'index'])->name('home');
-
-/* 
-Route::get('/home', [HomeController::class, 'index'])->name('home')->middleware('auth');
-Route::get('/pages/masterclass/create',[MasterclassController::class,'create']); 
-
-CON EL MÉTODO RESOURCE YA SE TIENE ACCESO A TODOS LOS MÉTODOS DEL CONTROLADOR MasterclassController EN LA RUTA /pages/masterclass. No hace falta hacer ruta para create, delete, storage...
-*/
-
-Route::resource('/pages/masterclass',MasterclassController::class);
+Route::get('/', [MasterclassController::class, 'index'])->name('welcome');
