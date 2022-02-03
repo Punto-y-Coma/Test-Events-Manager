aa068aa5f4fd305beecfe5af0c3668de730d8b4f
elegarmi <elegarmi@gmail.com>
2022-02-03 12:40:17 +0100

    edit styles changed

--- resources/views/pages/admin.blade.php
+++ resources/views/pages/admin.blade.php
@@ -2,30 +2,29 @@
 
 @section('content')
 
-    <div class="container">
+<div class="container edit-masterclasses-container">
 
-    <h1 class="display-6 mb-5 section-title"><span class="section-title-line">MASTERCLASES DISPONIBLES</span></h1>
-
+    <h1 class="display-6 mb-5 section-title"><span class="section-title-line">Editar masterclasses</span></h1>
 
     <div>
-        <button type="button" class="btn btn-custom btn-gray" value="add_masterclass"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-plus" viewBox="0 0 20 20">
+        <button type="button" class="btn btn-custom btn-gray" value="add_masterclass"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
           </svg></button>
     </div>
 
-    <table class="table">
-        <thead class="thead-dark">
+    <table class="table edit-masterclasses-table">
+        <thead class="table-dark">
             <tr>
                 <th scope="col">#</th>
                 <th scope="col">Title</th>
                 <th scope="col">Date</th>
                 <th scope="col">Vacants</th>
                 <th scope="col">Description</th>
-                <th scope="col"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-check-square" viewBox="0 0 20 20">
+                <th scope="col"><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-check-square">
                     <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
                     <path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/>
                   </svg></th>
-                <th scope="col"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 20 20">
+                <th scope="col"><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-trash-fill">
                     <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                   </svg></th>
                 <th scope="col"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 20 20">
@@ -52,6 +51,6 @@
         {!! $masterclasses->links() !!}
     </div>
 
-    </div>
+</div>
 
 @endsection
