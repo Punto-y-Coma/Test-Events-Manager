eb186120d8fa2dbaefdce3bd91b9ea555d40de90
Raul <raulfernan000@gmail.com>
2022-02-10 11:47:39 +0100

    Header has been fixed

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -13,9 +13,6 @@
 <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
 <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
 
-<!-- Bootstrap bundle -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}" defer></script>
 
